0311b76a7e73bf1f6233a11d8979c43460caee9a
lion17s <serhii.sheptytskyi@gmail.com>
2016-03-27 15:12:57 +0300

    Fixed default name of test

--- src/main/java/utils/ReportManager.java
+++ src/main/java/utils/ReportManager.java
@@ -23,6 +23,7 @@ public class ReportManager {
 
 	public synchronized static Map<Long, ExtentTest> startTest(String testName, String testDescription, String ... groups) {
 		Long threadID = Thread.currentThread().getId();
+		
 		ExtentTest test = getInstance().startTest(testName, testDescription);
 		test.assignCategory(groups);
 		testThread.put(threadID, test);
@@ -48,9 +49,21 @@ public class ReportManager {
 	
 	private String getTestName(Method m) {
 		String testName = null;
+		String address = null;
+		String[] testGroups = m.getAnnotation(Test.class).groups();
+		for (int i = 0; i < testGroups.length; i++) {
+			if (testGroups[i].startsWith("http")) {
+				address = testGroups[i];
+			}
+		}
+		if (address != null) {
+			testName = "<a href=" + "\"" + address + "\""+ "target=_blank alt=This test is linked to test case. Click to open it>" + m.getAnnotation(Test.class).testName() + "</a>";
+		} else {
 			testName = m.getAnnotation(Test.class).testName();
-		if (testName == null || testName == "") {
-			testName = getClass().getSimpleName();
+		}
+
+		if (testName == null || testName.equals("")) {
+			testName = m.getName();
 		}
 		return testName;
 	}
@@ -69,8 +82,13 @@ public class ReportManager {
 		String[] testGroups = m.getAnnotation(Test.class).groups();
 		try {
 			for (int i = 0; i < testGroups.length; i++) {
-				b = b + ": " + testGroups[i];
+				if (testGroups[i].startsWith("http")) {
+					continue;
+				} else {
+					b = b + " " + testGroups[i] + "; ";				}
+
 			}
+			b = b.substring(0, b.length()-2);
 		} catch (Exception e) {
 		}
 		return b;
