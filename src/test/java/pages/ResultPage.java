aba385bea3efbb445a1aac7b692a7d0f397874a4
lion17s <serhii.sheptytskyi@gmail.com>
2016-03-27 13:34:00 +0300

    Added result Page

---
+++ src/test/java/pages/ResultPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class ResultPage {
+	
+	public static final String airlineCards = "//div[@class='airline-card']//div[@class='title']";
+//	public static final String ticketPrice = "//button[@aria-label='Select']";
+
+}
